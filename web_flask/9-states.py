43dab5366d5a1ba761a13761934784b5f138238e
Kwizera Emmanuel <51673007+Emmanuel-kwizera@users.noreply.github.com>
2024-10-12 19:23:08 +0200

    refact: 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,36 +1,28 @@
 #!/usr/bin/python3
+"""
+start Flask application
+"""
 
-"""Starts a Flask web application"""
-
+from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-from flask import Flask
-from flask import render_template
-
 app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-def states():
-    """Comment"""
-    return render_template('9-states.html',
-                           states=storage.all('State').values())
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """Remove the current SQLAlchemy Session"""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-@app.route('/states/<id>', strict_slashes=False)
-def states_id(id):
-    """Comment"""
-    for state in storage.all(State).values():
-	if state.id == id:
-	    return render_template('9-states.html', state=state)
-    return render_template('9-states.html')
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port='5000')
